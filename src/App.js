f7a3fcf6aec1c2d8f1c8d4e382f5543d661e9f4b
jairovera17 <jairovera17@gmail.com>
2020-08-02 18:53:58 -0500

    React.createElement

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hi i'm in react</h1>
-    </div>
+    // <div className="App">
+    //   <h1>Hi i'm in react</h1>
+    // </div>
+    React.createElement("div", {className: "App"}, React.createElement("h1", null, "Hi i'm in react"))
   );
 }
 
