24dd7bbff332f1c11da767b8109e7b9b9f7048dc
jairovera17 <jairovera17@gmail.com>
2020-08-03 11:28:00 -0500

    Styling components

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,10 @@
 import React from "react"
+import Person from "./Person.css"
 
 //Presentation component - only show info
 const person = (props) => {
     const {name, age, click, changed, children} = props
-    return (<div>
+    return (<div className="Person">
         <p onClick={click}>I'm a {name} and I am {age} years old</p>
         <p>{children}</p>
         <input type="text" onChange={changed} value={name}/>
