75199d7ea664974174559a81f87fc88d736cb7b2
jairovera17 <jairovera17@gmail.com>
2020-08-02 19:25:49 -0500

    First component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import Person from "./Person/Person"
 
 function App() {
   return (
-    // <div className="App">
-    //   <h1>Hi i'm in react</h1>
-    // </div>
-    React.createElement("div", {className: "App"}, React.createElement("h1", null, "Hi i'm in react"))
+    <div className="App">
+      <h1>Hi i'm in react</h1>
+      <Person />
+      <Person />
+      <Person />
+    </div>
   );
 }
 
